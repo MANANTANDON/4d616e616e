a748dce1465c7d55b79d4532dc8dd1d2b80dab17
mananTandon <manantandon@gmail.com>
2025-03-04 01:39:37 +0530

    charing issue..

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Apple, WifiRounded } from "@mui/icons-material";
 import { Box, Grid, Typography } from "@mui/material";
 import { GetCurrentDayAndTime } from "@/hooks/getCurrentDayAndTime";
 import React from "react";
-// import { Charging } from "@/components/Misc/Charging";
+import { Charging } from "@/components/Misc/Charging";
 
 export const Header: React.FC = () => {
   const LeftMenu = ["Finder", "File", "Edit", "View", "Go", "Window", "Help"];
@@ -50,7 +50,7 @@ export const Header: React.FC = () => {
             }}
           >
             <WifiRounded fontSize="small" sx={{ color: "white" }} />
-            {/* <Charging /> */}
+            <Charging />
             <Typography className="menuFamily">
               {GetCurrentDayAndTime()}
             </Typography>
