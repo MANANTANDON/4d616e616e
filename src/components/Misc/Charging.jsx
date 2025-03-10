2863f0f0521331012c655be6079b8dcd2c480874
Manan Tandon <manantandon@Manans-MacBook-Air.local>
2025-03-10 21:53:50 +0530

    changes...

--- src/components/Misc/Charging.jsx
+++ src/components/Misc/Charging.jsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Box, Divider, Menu, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 const useBatteryStatus = () => {
@@ -44,16 +44,102 @@ const useBatteryStatus = () => {
 export const Charging = () => {
   const { level, charging } = useBatteryStatus();
 
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div>
       {level !== null ? (
-        <Typography
-          className="sfpro"
-          sx={{ fontSize: "14px", display: "flex", alignItems: "center" }}
-        >
-          <span style={{ fontSize: "12px" }}>{level}% </span>
-          {charging ? "􀢋" : "􀺸"}
-        </Typography>
+        <>
+          <Typography
+            className="sfpro"
+            sx={{
+              fontSize: "14px",
+              display: "flex",
+              alignItems: "center",
+              cursor: "default",
+            }}
+            onClick={handleClick}
+            id="charging-icon"
+            aria-controls={open ? "charging-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+          >
+            <span style={{ fontSize: "12px" }}>{level}% </span>
+            {charging
+              ? "􀢋"
+              : level <= 25
+              ? "􀛩"
+              : level <= 50
+              ? "􀺶"
+              : level <= 75
+              ? "􀺸"
+              : "􀛨"}
+          </Typography>
+          <Menu
+            id="charging-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "charging-icon",
+            }}
+            PaperProps={{
+              sx: {
+                mt: "4px",
+                ml: "-150px",
+                py: "0px !important ",
+                width: "230px",
+                bgcolor: "rgba( 0, 0, 0, 0.5 )",
+                boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+                backdropFilter: "blur( 20px )",
+                border: "1px solid rgba( 255, 255, 255, 0.3 )",
+                borderRadius: "6px",
+              },
+            }}
+            sx={{
+              "& .MuiList-root.MuiMenu-list": {
+                paddingTop: 0,
+                paddingBottom: 0,
+              },
+            }}
+          >
+            <Box
+              sx={{
+                p: 1.5,
+              }}
+            >
+              <Typography
+                className="sfpro"
+                sx={{ color: "#FFFFFF", fontSize: "14px" }}
+              >
+                Battery
+              </Typography>
+              <Typography
+                className="sfpro"
+                sx={{ color: "#FFFFFF90", fontSize: "12px" }}
+              >
+                Power Source: Power Adpater
+              </Typography>
+              <hr style={{ border: "0.5px solid #FFFFFF90" }} />
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              ></Box>
+            </Box>
+          </Menu>
+        </>
       ) : (
         <Typography sx={{ fontSize: "14px" }}>
           Battery info not available
