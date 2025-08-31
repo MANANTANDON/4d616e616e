dd0e534322788398017ab8d1c53bbbe96ccf300d
mananTandon <manantandon@gmail.com>
2025-08-31 18:53:14 +0530

    ios 26 ui desing...

--- src/components/Mobile/LinkModal.jsx
+++ src/components/Mobile/LinkModal.jsx
@@ -2,17 +2,19 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import { Button } from "@mui/material";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  bgcolor: "#2B2B2B",
-  borderRadius: "14px",
+  bgcolor: "#D7EDF5",
+  borderRadius: "30px",
   outline: "none",
-  width: "270px",
+  width: "calc(100% - 70px)",
+  py: "22px",
+  pb: "16px",
+  px: "16px",
 };
 
 export const LinkModal = ({ clickLink, open, setOpen }) => {
@@ -23,28 +25,42 @@ export const LinkModal = ({ clickLink, open, setOpen }) => {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Box sx={{ py: "20px", px: "16px" }}>
+        <Box sx={style} className="liquidGlass">
+          <Box sx={{ px: "12px" }}>
             <Typography
-              textAlign="center"
               className="sfpro"
-              sx={{ color: "#ffffff", fontSize: "17px" }}
+              sx={{ color: "#000000", fontSize: "17px", fontWeight: "bold" }}
             >
-              Open "{clickLink?.name}"
+              Open "{clickLink?.name}"?
             </Typography>
           </Box>
-          <Box>
+          <Box sx={{ px: "12px", pt: "9px" }}>
+            <Typography
+              className="sfpro"
+              sx={{ color: "#00000090", fontSize: "17px", fontWeight: "bold" }}
+            >
+              You are trying to open {clickLink?.name}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              mt: "22px",
+              display: "flex",
+              flexDirection: "column",
+              gap: "9px",
+            }}
+          >
             <a href={clickLink?.link} target="_blank">
               <Box
                 className="sfpro"
                 sx={{
+                  borderRadius: "28px",
                   textAlign: "center",
-                  py: "11px",
+                  py: "18px",
                   fontSize: "17px",
-                  color: "#007AFF",
+                  color: "#000000",
                   fontWeight: "bold",
-                  borderTop: "1px solid #ffffff24",
-                  borderBottom: "1px solid #ffffff24",
+                  bgcolor: "#00000020",
                 }}
               >
                 Allow
@@ -53,10 +69,13 @@ export const LinkModal = ({ clickLink, open, setOpen }) => {
             <Box
               className="sfpro"
               sx={{
+                borderRadius: "28px",
                 textAlign: "center",
-                py: "11px",
+                py: "18px",
                 fontSize: "17px",
-                color: "#007AFF",
+                color: "#000000",
+                fontWeight: "bold",
+                bgcolor: "#00000020",
               }}
               onClick={() => setOpen(false)}
             >
