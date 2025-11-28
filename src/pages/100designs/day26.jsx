35db82d72c78efd53eceea21c554495421bd9275
mananTandon <manantandon@gmail.com>
2025-11-28 13:06:19 +0530

    Day 36 wrapup...

--- src/pages/100designs/day26.jsx
+++ src/pages/100designs/day26.jsx
@@ -1,20 +1,45 @@
-import { Container, Divider, Grid2 } from "@mui/material";
+import { Divider, Grid2, useMediaQuery } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 
 export default function Day26() {
-  const handleSubmit = () => {};
+  const laptop = useMediaQuery("(min-width:769px)");
+
+  const handleSubmit = () => {
+    alert("User Logged in !");
+  };
+
+  const LoginWithGoogle = () => {
+    alert("Logged in google !");
+  };
   return (
     <>
-      <div className="h-screen w-screen bg-[#FFFFFF] relative">
-        <div className="absolute top-0 left-0 right-0 border-b border-[#ECECEC] h-[38px]" />
-        <div className="absolute bottom-0 left-0 right-0 border-t border-[#ECECEC] h-[38px]" />
-        <Container maxWidth="xl">
+      {!laptop && (
+        <div className="flex items-center justify-center h-screen">
+          <div className=" border border-slate-600 bg-slate-800 p-5 text-zinc-100 rounded-3xl">
+            Switch to Desktop
+          </div>
+        </div>
+      )}
+      {laptop && (
+        <div className="h-screen w-screen bg-[#FFFFFF] relative">
+          <div className="absolute top-0 left-0 right-0 border-b border-[#ECECEC] h-[38px]" />
+          <div className="absolute bottom-0 left-0 right-0 border-t border-[#ECECEC] h-[38px]" />
+          <div className="absolute bottom-0 left-[38px] top-0 border-l border-[#ECECEC]" />
+          <div className="absolute top-0 bottom-0 left-[calc(50%-38px)] border-l border-[#ECECEC]" />
+
           <Grid2 container>
             <Grid2
               size={6}
-              className="border-l border-[#ECECEC] h-screen flex flex-col items-center justify-center"
+              className="h-screen flex flex-col items-center justify-center relative"
             >
+              <Image
+                src={"/images/designs/day26grad.png"}
+                layout="intrinsic"
+                height={141}
+                width={642}
+                className="absolute bottom-0"
+              />
               <Image
                 src={"/images/designs/day26logo.png"}
                 layout="intrinsic"
@@ -31,7 +56,7 @@ export default function Day26() {
                 </a>
               </div>
               <form onSubmit={handleSubmit} className="mt-8">
-                <div className="w-[450px] max-w-[450px]">
+                <div className="w-[350px] max-w-[350px] xl:w-[450px] xl:max-w-[450px]">
                   <div className="sfpro-text text-[#3C3C3C] font-medium mb-3">
                     Enter your email
                   </div>
@@ -42,7 +67,7 @@ export default function Day26() {
                     className="w-full border border-[#E5E5E5] sfpro-text p-3 rounded-lg outline-0 text-[#383838] font-light"
                   />
                 </div>
-                <div className="w-[450px] max-w-[450px] mt-5">
+                <div className="w-[350px] max-w-[350px] xl:w-[450px] xl:max-w-[450px] mt-5">
                   <div className="sfpro-text text-[#3C3C3C] font-medium mb-3">
                     Password
                   </div>
@@ -60,12 +85,15 @@ export default function Day26() {
                   Continue
                 </button>
               </form>
-              <div className="w-[450px] max-w-[450px] my-8">
+              <div className="w-[350px] max-w-[350px] xl:w-[450px] xl:max-w-[450px] my-8">
                 <Divider className="sfpro-text font-extralight text-[#4C4C4C]">
                   <span className="px-16">OR</span>
                 </Divider>
               </div>
-              <button className="p-3 border border-[#D7D7D7] w-[450px] max-w-[450px] sfpro-text text-[#3D3D3D] rounded-lg  font-medium cursor-pointer flex items-center justify-center gap-4">
+              <button
+                className="p-3 border border-[#D7D7D7] w-[350px] max-w-[350px] xl:w-[450px] xl:max-w-[450px] sfpro-text text-[#3D3D3D] rounded-lg  font-medium cursor-pointer flex items-center justify-center gap-4"
+                onClick={LoginWithGoogle}
+              >
                 <Image
                   src={"/images/designs/google.png"}
                   layout="intrinsic"
@@ -84,8 +112,8 @@ export default function Day26() {
               />
             </Grid2>
           </Grid2>
-        </Container>
-      </div>
+        </div>
+      )}
     </>
   );
 }
