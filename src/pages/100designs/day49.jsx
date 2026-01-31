cb101c91ea8a2e7699872f17775f58f94ddf9a3d
mananTandon <manantandon@gmail.com>
2026-01-31 19:09:08 +0530

    Day 49 images set

--- src/pages/100designs/day49.jsx
+++ src/pages/100designs/day49.jsx
@@ -5,10 +5,10 @@ const Day49 = () => {
   const MENU = ["Work", "Services", "Approach", "Studio", "Journal"];
 
   const IMG = [
-    { name: "/images/designs/day49/1.png", height: 30, width: 30 },
-    { name: "/images/designs/day49/2.png", height: 35, width: 84 },
-    { name: "/images/designs/day49/3.png", height: 32, width: 32 },
-    { name: "/images/designs/day49/4.png", height: 27, width: 118 },
+    { name: "/images/designs/day49/10.png", height: 30, width: 40 },
+    { name: "/images/designs/day49/20.png", height: 35, width: 84 },
+    { name: "/images/designs/day49/30.png", height: 32, width: 40 },
+    { name: "/images/designs/day49/40.png", height: 10, width: 150 },
   ];
 
   const CTAButton = ({ text }) => (
@@ -60,16 +60,21 @@ const Day49 = () => {
             </button>
             <CTAButton text="See Selected Work" />
           </div>
-          <div className="flex gap-7">
-            {IMG.map((item, key) => (
-              <Image
-                key={key}
-                src={item.name}
-                height={item.height}
-                width={item.width}
-                layout="intrinsic"
-              />
-            ))}
+          <div className="flex flex-col gap-5 mt-10">
+            <div className="sfpro-text text-zinc-100/80 text-xl font-medium">
+              Our Partners:
+            </div>
+            <div className="flex gap-7">
+              {IMG.map((item, key) => (
+                <Image
+                  key={key}
+                  src={item.name}
+                  height={item.height}
+                  width={item.width}
+                  layout="intrinsic"
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
