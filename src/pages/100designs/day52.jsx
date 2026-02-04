e218ef690fd850bc28c69bcc345512b81c2055bf
mananTandon <manantandon@gmail.com>
2026-02-04 12:15:23 +0530

    Day 52 constraints added...

--- src/pages/100designs/day52.jsx
+++ src/pages/100designs/day52.jsx
@@ -8,8 +8,13 @@ const Day52 = () => {
 
   return (
     <>
+      <div className="flex items-center justify-center h-screen lg:hidden">
+        <div className=" border border-slate-600 bg-slate-800 p-5 text-zinc-100 rounded-3xl">
+          Switch to Desktop
+        </div>
+      </div>
       <div
-        className="h-screen w-screen bg-zinc-800 p-[60px]"
+        className="h-screen w-screen bg-zinc-800 p-[60px] hidden lg:block"
         style={{
           backgroundImage: "url(/images/designs/day52/bg.png)",
           backgroundSize: "cover",
